1f5ef4b6b6bbd500404849df3704d49fb002fff5
rooandqoo <sa2ki.k32@gmail.com>
2015-06-07 14:47:11 +0900

    initial commit

---
+++ wear/src/main/java/com/rooandqoo/weartest/MainActivity.java
@@ -0,0 +1,50 @@
+package com.rooandqoo.weartest;
+
+import android.app.Activity;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.support.wearable.view.DotsPageIndicator;
+import android.support.wearable.view.GridViewPager;
+import android.support.wearable.view.WatchViewStub;
+import android.view.View;
+import android.view.WindowInsets;
+
+public class MainActivity extends Activity {
+
+    private GridViewPager mGridViewPager;
+    private DotsPageIndicator mDotsPageIndicator;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mGridViewPager = (GridViewPager) stub.findViewById(R.id.pager);
+                mDotsPageIndicator = (DotsPageIndicator) stub.findViewById(R.id.page_indicator);
+                setupViewPager();
+            }
+        });
+    }
+
+    private void setupViewPager() {
+        final Resources res = getResources();
+        mGridViewPager.setOnApplyWindowInsetsListener(new View.OnApplyWindowInsetsListener() {
+            @Override
+            public WindowInsets onApplyWindowInsets(View view, WindowInsets insets) {
+                final boolean round = insets.isRound();
+                int rowMargin = res.getDimensionPixelOffset(R.dimen.page_row_margin);
+                int colMargin = res.getDimensionPixelOffset(round ?
+                        R.dimen.page_column_margin_round : R.dimen.page_column_margin);
+                mGridViewPager.setPageMargins(rowMargin, colMargin);
+                mGridViewPager.onApplyWindowInsets(insets);
+                return insets;
+            }
+        });
+
+        mGridViewPager.setAdapter(new MyGridViewPagerAdapter(this, getFragmentManager()));
+        mDotsPageIndicator.setPager(mGridViewPager);
+    }
+}
---
+++ wear/src/main/java/com/rooandqoo/weartest/MyGridViewPagerAdapter.java
@@ -0,0 +1,47 @@
+package com.rooandqoo.weartest;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.Context;
+import android.support.wearable.view.CardFragment;
+import android.support.wearable.view.FragmentGridPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyGridViewPagerAdapter extends FragmentGridPagerAdapter {
+
+    private final Context mContext;
+    private List<Fragment> mColumns;
+
+    public MyGridViewPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+
+        mColumns = new ArrayList<>();
+        mColumns.add(cardFragment("hoge", "fuga"));
+        mColumns.add(cardFragment("piyo", "poyo"));
+        mColumns.add(new CustomFragment());
+        mColumns.add(cardFragment("fizz", "buzz"));
+    }
+
+    private Fragment cardFragment(String title, String text) {
+        CardFragment fragment = CardFragment.create(title, text);
+        fragment.setCardMarginBottom(mContext.getResources().getDimensionPixelSize(R.dimen.card_margin_bottom));
+        return fragment;
+    }
+
+    public Fragment getFragment(int i, int i1) {
+        return mColumns.get(i1);
+    }
+
+    @Override
+    public int getRowCount() {
+        return 1;
+    }
+
+    @Override
+    public int getColumnCount(int i) {
+        return mColumns.size();
+    }
+}
